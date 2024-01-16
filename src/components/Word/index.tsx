92953f194bfeeb2c86bcda82473aa04518547155
Roman Ponomarev <maksugr@gmail.com>
2024-01-16 17:01:37 +0100

    Add quote styles

--- src/components/Word/index.tsx
+++ src/components/Word/index.tsx
@@ -7,7 +7,7 @@ export const Word: React.FC<IWordProps> = ({ word }) => {
 
     return (
         <div className="bg-[#facc15] border-4 border-[#000] rounded-lg p-6 drop-shadow-3xl">
-            <div className="flex flex-col justify-between items-start gap-4">
+            <div className="flex flex-col justify-between items-start gap-4 mdx-content">
                 <p className="mt-4 outfit text-2xl md:text-5xl lg:text-7xl">
                     <a href={`/words/${slug}`}>{frontmatter.title}</a>
                 </p>

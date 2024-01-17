88d2caba712c6bdab4fd71dcf7e77a76b55b27f4
Roman Ponomarev <maksugr@gmail.com>
2024-01-17 19:16:44 +0100

    Add force-dynamic for random word route

--- src/app/words/api/random/route.ts
+++ src/app/words/api/random/route.ts
@@ -1,5 +1,7 @@
 import { getRandomWord } from "../../../../utils/words";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
     const { slug } = await getRandomWord();
     return Response.json({ slug });

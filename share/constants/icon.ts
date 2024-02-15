f66cfc5924b3b12f27ec97cd664a006add980dc1
Roman Ponomarev <maksugr@gmail.com>
2024-02-15 22:50:31 +0100

    Rename icon size

--- share/constants/icon.ts
+++ share/constants/icon.ts
@@ -1,2 +1,2 @@
-export const HEADER_ICON_SIZE = 24;
+export const BASE_ICON_SIZE = 24;
 export const WORD_ICON_SIZE = 16;
--- src/components/ui/Search/ShuffleIcon.tsx
+++ src/components/ui/Search/ShuffleIcon.tsx
@@ -1,12 +1,12 @@
-import { ICON_SIZE } from '../../../../share/constants/icon';
+import { BASE_ICON_SIZE } from '../../../../share/constants/icon';
 
 export const ShuffleIcon = () => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 32 32"
-            width={ICON_SIZE}
-            height={ICON_SIZE}
+            width={BASE_ICON_SIZE}
+            height={BASE_ICON_SIZE}
         >
             <path
                 d="M22.59,19.41,26.17,23H19.55l-4.37-7,4.37-7h6.62l-3.58,3.59L24,14l6-6L24,2,22.59,3.41,26.17,7H19.55a2,2,0,0,0-1.69.94L14,14.11,10.14,7.94A2,2,0,0,0,8.45,7H2V9H8.45l4.37,7L8.45,23H2v2H8.45a2,2,0,0,0,1.69-.94L14,17.89l3.86,6.17a2,2,0,0,0,1.69.94h6.62l-3.58,3.59L24,30l6-6-6-6Z"
@@ -14,8 +14,8 @@ export const ShuffleIcon = () => {
             />
             <rect
                 id="_Transparent_Rectangle_"
-                width={ICON_SIZE}
-                height={ICON_SIZE}
+                width={BASE_ICON_SIZE}
+                height={BASE_ICON_SIZE}
                 fill="none"
             />
         </svg>

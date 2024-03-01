11b055915eebf83b3571db097133b4fd0991927f
Roman Ponomarev <maksugr@gmail.com>
2024-03-01 23:19:55 +0100

    Add timeout

--- src/app/words/api/your-language-text/route.ts
+++ src/app/words/api/your-language-text/route.ts
@@ -28,26 +28,29 @@ export async function GET(request: NextRequest) {
     if (!text) {
         const openai = new OpenAI({ apiKey: CONFIG.openai.apiKey });
 
-        const completion = await openai.chat.completions.create({
-            model: 'gpt-4-turbo-preview',
-            messages: [
-                {
-                    role: 'system',
-                    content: `You are a helpful AI assistant, with expertise in ${language} proverbs and slang.`,
-                },
-                {
-                    role: 'user',
-                    content: `
+        const completion = await openai.chat.completions.create(
+            {
+                model: 'gpt-4-turbo-preview',
+                messages: [
+                    {
+                        role: 'system',
+                        content: `You are a helpful AI assistant, with expertise in ${language} proverbs and slang.`,
+                    },
+                    {
+                        role: 'user',
+                        content: `
 You will get a german proverb or slang in German language.
 Proverb or Slang: ${title}.
 Try to understand its meaning in German language and answer me with the most suitable and similar proverb or slang in ${language} language. 
 Add more details. 
 You can speak ONLY ${language} language.
 `,
-                },
-            ],
-            temperature: 0.00000001,
-        });
+                    },
+                ],
+                temperature: 0.00000001,
+            },
+            { timeout: 60000 * 5, maxRetries: 5 }
+        );
 
         const openaiText = completion.choices[0].message.content || '';
 

43e1e9560f183e799653e0a293530db31b39b633
Roman Ponomarev <maksugr@gmail.com>
2025-02-17 19:21:20 +0100

    Change black color

--- share/constants/colors.ts
+++ share/constants/colors.ts
@@ -1,7 +1,7 @@
 import { ColorEnum } from '../enums/color';
 
 export const COLORS = {
-    [ColorEnum.MAIN_BLACK]: '#212121',
+    [ColorEnum.MAIN_BLACK]: '#1e3a8a',
     [ColorEnum.MAIN_WHITE]: '#f9eef2',
     [ColorEnum.MAIN_BLUE]: '#388df8',
     [ColorEnum.MAIN_YELLOW]: '#facc15',

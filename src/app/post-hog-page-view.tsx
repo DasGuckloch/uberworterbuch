87f7443cb2d126ec247a6d650992465c4a7db92e
Roman Ponomarev <maksugr@gmail.com>
2025-02-18 18:43:52 +0100

    Add dynamic

--- src/app/post-hog-page-view.tsx
+++ src/app/post-hog-page-view.tsx
@@ -4,7 +4,7 @@ import { usePathname, useSearchParams } from 'next/navigation';
 import { useEffect, Suspense } from 'react';
 import { usePostHog } from 'posthog-js/react';
 
-function PostHogPageView() {
+function PostHogPageView(): null {
     const pathname = usePathname();
     const searchParams = useSearchParams();
     const posthog = usePostHog();
@@ -14,7 +14,7 @@ function PostHogPageView() {
         if (pathname && posthog) {
             let url = window.origin + pathname;
             if (searchParams.toString()) {
-                url = url + '?' + searchParams.toString();
+                url = url + `?${searchParams.toString()}`;
             }
 
             posthog.capture('$pageview', { $current_url: url });
@@ -24,7 +24,7 @@ function PostHogPageView() {
     return null;
 }
 
-// Wrap this in Suspense to avoid the useSearchParams usage above
+// Wrap this in Suspense to avoid the `useSearchParams` usage above
 // from de-opting the whole app into client-side rendering
 // See: https://nextjs.org/docs/messages/deopted-into-client-rendering
 export default function SuspendedPostHogPageView() {
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,16 @@
-// app/providers.jsx
 'use client';
+
+import dynamic from 'next/dynamic';
 import posthog from 'posthog-js';
 import { PostHogProvider as PHProvider } from 'posthog-js/react';
 import { useEffect } from 'react';
-
-import PostHogPageView from './post-hog-page-view';
 
 export function PostHogProvider({ children }: { children: any }) {
     useEffect(() => {
         posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as any, {
             api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-            person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
             capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+            capture_pageleave: true, // Enable pageleave capture
         });
     }, []);
 
@@ -22,3 +21,7 @@ export function PostHogProvider({ children }: { children: any }) {
         </PHProvider>
     );
 }
+
+const PostHogPageView = dynamic(() => import('./post-hog-page-view'), {
+    ssr: false,
+});
